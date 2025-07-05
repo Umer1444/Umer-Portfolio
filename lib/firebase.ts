57f952334c1cc3ad97567414ef5de83d3924322d
Umer Shaikh <umershaikh1444@gmail.com>
2025-07-05 20:39:38 +0530

    upadate fire

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -13,13 +13,13 @@ import type { Firestore } from "firebase/firestore"
 import { collection, addDoc, serverTimestamp, doc, deleteDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMgxa32ZeImHf9eNawFdHx5RzEXd0CQD8",
-  authDomain: "umer-portfolio-50eae.firebaseapp.com",
-  projectId: "umer-portfolio-50eae",
-  storageBucket: "umer-portfolio-50eae.appspot.com",
-  messagingSenderId: "858091244883",
-  appId: "1:858091244883:web:7fdb07a500853455f3a5a6",
-  measurementId: "G-6V9974LYN0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 }
 
 // ---------- initialise / reuse the Firebase app ----------
